ab6dd13ec0fd55cc93a1753494a4fade89dd204c
Yurii-Borys <you@example.com>
2021-04-01 15:19:25 +0300

    Ex1,2

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+    public static void main (String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
---
+++ Ex2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+    Scanner question=new Scanner(System.in);
+    System.out.println("How are you?");
+    String answer=question.nextLine();
+    System.out.println("You are " + answer);
+    }
+}
