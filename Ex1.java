bf0360efab5165da816ce48d047613f4f35a5da9
AndriiTokarchuk <andriitokarch@gmail.com>
2021-04-19 12:48:13 +0300

    Update Ex1.java
    // Suggestions:
    // 1. Please, read tasks more attentively. In task it is said - Read values a and b from Console
    // 2. Please add initial conditions in each task as commented text.  So I could understand what should be done in each task

--- Ex1.java
+++ Ex1.java
@@ -1,3 +1,9 @@
+//1) define integer variables a and b. Read values a and b from Console and calculate: a+b, a-b, a*b, a/b. Output obtained results.
+
+// Suggestions:
+// Please, read tasks more attentively
+// Read values a and b from Console
+
 public class Ex1 {
     public static void main (String[] args) {
         int a=10;
